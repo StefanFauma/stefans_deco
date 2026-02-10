330f3c17d601da36c14c40b4d4490b839da023ab
mat the genius <mat.flammini@gmail.com>
2026-02-10 12:05:33 +0100

    1.4
    Ich habe einen neuen Creative Tab hinzugefügt leider ohne Textur und ohne richtige Items aber trotzdem :D

---
+++ src/main/java/stefans_deco/stefans_deco/BlocksAndItems/CreativeDecoTab.java
@@ -0,0 +1,31 @@
+package stefans_deco.stefans_deco.BlocksAndItems;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import stefans_deco.stefans_deco.Stefans_deco;
+
+import java.util.function.Supplier;
+
+public class CreativeDecoTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Stefans_deco.MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> DECO_ITEMS_TAB =
+            CREATIVE_MODE_TAB.register("deco_items", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("itemGroup.stefans_deco"))
+                            .icon(() -> new ItemStack(ModItems.DECO.get()))
+                            .displayItems((params, output) -> {
+                                output.accept(ModItems.DECO.get());
+                            })
+                            .build()
+            );
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
--- src/main/java/stefans_deco/stefans_deco/Stefans_deco.java
+++ src/main/java/stefans_deco/stefans_deco/Stefans_deco.java
@@ -1,36 +1,16 @@
 package stefans_deco.stefans_deco;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
+import stefans_deco.stefans_deco.BlocksAndItems.CreativeDecoTab;
+import stefans_deco.stefans_deco.BlocksAndItems.ModItems;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Stefans_deco.MODID)
@@ -56,8 +36,8 @@ public class Stefans_deco {
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
+        ModItems.register(modEventBus);
+        CreativeDecoTab.register(modEventBus);
 
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
