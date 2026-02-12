07cb7b28be6e57738e6f127dd5c815ee792729a3
StefanFauma <stefan.fauma@gmail.com>
2026-02-12 14:14:59 +0100

    1.5
    Creative Tab existiert und reflectiveglass textur gefixt.

--- src/main/java/stefans_deco/stefans_deco/BlocksAndItems/ColorBlocks/redblock.java
+++ src/main/java/stefans_deco/stefans_deco/BlocksAndItems/ColorBlocks/redblock.java
@@ -1,4 +1,4 @@
-package stefans_deco.stefans_deco.BlocksAndItems;
+package stefans_deco.stefans_deco.BlocksAndItems.ColorBlocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
--- src/main/java/stefans_deco/stefans_deco/Stefans_deco.java
+++ src/main/java/stefans_deco/stefans_deco/Stefans_deco.java
@@ -2,14 +2,18 @@ package stefans_deco.stefans_deco;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
-import stefans_deco.stefans_deco.BlocksAndItems.CreativeDecoTab;
+//import stefans_deco.stefans_deco.BlocksAndItems.CreativeDecoTab;
+import stefans_deco.stefans_deco.BlocksAndItems.globe;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Stefans_deco.MODID)
@@ -25,6 +29,16 @@ public class Stefans_deco {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "stefans_deco" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> DECO_TAB =
+            CREATIVE_MODE_TABS.register("stefans_tab", () ->
+                            CreativeModeTab.builder()
+                                    .title(Component.translatable("creativetab.stefans_deco"))
+                                    .icon(() -> new ItemStack(globe.GLOBE_ITEM.get()))
+                                    .displayItems((parameters, output) -> {
+                                        output.accept(globe.GLOBE_ITEM.get());
+                                    })
+                                    .build()
+            );
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Stefans_deco(IEventBus modEventBus, ModContainer modContainer) {
@@ -35,8 +49,7 @@ public class Stefans_deco {
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so tabs get registered
-        CreativeDecoTab.register(modEventBus);
-
+        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
